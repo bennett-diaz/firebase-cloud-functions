454e1bcadc635b3ece118c6cc44bde5b554c17ed
Bennett Diaz <97626907+bennett-diaz@users.noreply.github.com>
2024-01-09 16:45:54 -0600

    add keepWarm

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Overview
 
 Repo for Firebase Cloud functions that are helpers across web applications
+
+## Commands
+ - In the directory that holds index.jx, run *firebase deploy*
---
+++ functions/index.js
@@ -0,0 +1,41 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const axios = require("axios");
+
+const testUrl = "https://backend-instacap.onrender.com/api/test";
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello world suceeded", {structuredData: true});
+  response.send("Hello world!");
+});
+
+exports.triggerWarm = onRequest(async (request, response) => {
+  try {
+    const res = await axios.get(testUrl);
+    logger.log("On-demand request successful, response:", res.data);
+    response.send({message: "On-demand request successful", data: res.data});
+  } catch (error) {
+    logger.error("Error pinging server:", error);
+  }
+});
+
+
+exports.keepServerWarmNew = onSchedule("every 14 minutes", async (data) => {
+  try {
+    const response = await axios.get(testUrl);
+    logger.log("Server pinged successfully, response:", response.data);
+  } catch (error) {
+    logger.error("Error pinging server:", error);
+  }
+});
+
