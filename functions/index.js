65f4b0a82a8fa1802b2190e9a03deefe15a45859
Bennett Diaz <97626907+bennett-diaz@users.noreply.github.com>
2024-01-15 20:52:59 -0600

    initial add of awaken onRequest

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,16 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onSchedule} = require("firebase-functions/v2/scheduler");
-const {onRequest} = require("firebase-functions/v2/https");
+const { onSchedule } = require("firebase-functions/v2/scheduler");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const axios = require("axios");
 
 const testUrl = "https://backend-instacap.onrender.com/api/test";
+const awakenUrl = "https://backend-instacap.onrender.com/api/awaken";
 
 exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello world suceeded", {structuredData: true});
+  logger.info("Hello world suceeded", { structuredData: true });
   response.send("Hello world!");
 });
 
@@ -23,7 +24,17 @@ exports.triggerWarm = onRequest(async (request, response) => {
   try {
     const res = await axios.get(testUrl);
     logger.log("On-demand request successful, response:", res.data);
-    response.send({message: "On-demand request successful", data: res.data});
+    response.send({ message: "On-demand request successful", data: res.data });
+  } catch (error) {
+    logger.error("Error pinging server:", error);
+  }
+});
+
+exports.triggerHF = onRequest(async (request, response) => {
+  try {
+    const res = await axios.post(awakenUrl, { sumModelId: "Salesforce/blip-image-captioning-base" });
+    logger.log("On-demand request successful, response:", res.data);
+    response.send({ message: "On-demand request successful", data: res.data });
   } catch (error) {
     logger.error("Error pinging server:", error);
   }
@@ -38,4 +49,7 @@ exports.keepServerWarmNew = onSchedule("every 14 minutes", async (data) => {
     logger.error("Error pinging server:", error);
   }
 });
+
+console.log('test')
+console.log('test back')
 
