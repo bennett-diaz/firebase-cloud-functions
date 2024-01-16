195c775167a9c866e15bfe53e0c175463d0d8f54
Bennett Diaz <97626907+bennett-diaz@users.noreply.github.com>
2024-01-15 21:45:01 -0600

    add cron job for wake HF

--- functions/index.js
+++ functions/index.js
@@ -6,50 +6,50 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-const { onSchedule } = require("firebase-functions/v2/scheduler");
-const { onRequest } = require("firebase-functions/v2/https");
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const axios = require("axios");
 
-const testUrl = "https://backend-instacap.onrender.com/api/test";
+const sumModel = "Salesforce/blip-image-captioning-base";
+// const testUrl = "https://backend-instacap.onrender.com/api/test";
 const awakenUrl = "https://backend-instacap.onrender.com/api/awaken";
+const awakenUrl = "https://backend-instacap.onrender.com/api/image/awaken";
+
 
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello world suceeded", { structuredData: true });
   response.send("Hello world!");
+  response.send(`Hello ${process.env.PLANET} and ${process.env.AUDIENCE}`);
 });
 
-exports.triggerWarm = onRequest(async (request, response) => {
-  try {
-    const res = await axios.get(testUrl);
-    logger.log("On-demand request successful, response:", res.data);
-    response.send({ message: "On-demand request successful", data: res.data });
-  } catch (error) {
-    logger.error("Error pinging server:", error);
-  }
-});
 
 exports.triggerHF = onRequest(async (request, response) => {
   try {
-    const res = await axios.post(awakenUrl, { sumModelId: "Salesforce/blip-image-captioning-base" });
-    logger.log("On-demand request successful, response:", res.data);
-    response.send({ message: "On-demand request successful", data: res.data });
+    const res = await axios.post(awakenUrl, {sumModelId: sumModel});
+    logger.log("HF on-demand request successful, response:", res.data);
+    response.send({message: "HF on-demand request successful", data: res.data});
   } catch (error) {
-    logger.error("Error pinging server:", error);
+    logger.error("Error pinging /awaken endpoint:", error);
+    response.status(500).send({error: "Error pinging /awaken endpoint"});
   }
 });
 
-
-exports.keepServerWarmNew = onSchedule("every 14 minutes", async (data) => {
+exports.keepHfWarm = onSchedule("every 14 minutes", async (data) => {
   try {
-    const response = await axios.get(testUrl);
-    logger.log("Server pinged successfully, response:", response.data);
+    const res = await axios.post(awakenUrl, {sumModelId: sumModel});
+    logger.log("HF wake request successful, response:", res.data);
   } catch (error) {
-    logger.error("Error pinging server:", error);
+    logger.error("Error with HF wake cron job:", error);
   }
 });
 
-console.log('test')
-console.log('test back')
-
+// also firebase remoteconfig, generate an image
+// exports.keepBackendWarm = onSchedule("every 14 minutes", async (data) => {
+//   try {
+//     const res = await axios.get(testUrl);
+//     logger.log("Server pinged successfully, response:", res.data);
+//   } catch (error) {
+//     logger.error("Error pinging server for keepBackendWarm:", error);
+//   }
+// });
